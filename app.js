e46a0be79483c3a918cdf88901df942da16f7dd9
razahamza0308 <razahamza0308@gmail.com>
2021-04-24 00:40:35 +0500

    cs

---
+++ app.js
@@ -0,0 +1,135 @@
+                // ********Question 1&2*********
+
+// var num1=3;
+// var num2=5;
+// var sum=num1+num2;
+// var sub=num1-num2;
+// var mul=num1*num2;
+// var div=num1/num2;
+// document.write("Sum of "+num1 +" and "+ num2 +" is " + sum +"<br>");
+// document.write("Substraction of "+num1 +" and "+ num2 +" is " + sub +"<br>");
+// document.write("Multiplication of "+num1 +" and "+ num2 +" is " + mul +"<br>");
+// document.write("Division of "+num1 +" and "+ num2 +" is " + div);
+
+
+
+                // ********Question 3*********
+
+// var a;
+// document.write("Value after variable decleration is:??" +"<br>");
+// a=5;
+// document.write("Initial value is "+a +"<br>");
+// a++;
+// document.write("Value after increament is "+a +"<br>");
+// a=a+7;
+// document.write("Value after addition is "+a +"<br>");
+// a--;
+// document.write("Value after decreament is "+a +"<br>");
+// a=a%3;
+// document.write("The remainder is:"+a +"<br><br>");
+
+
+                 // ********Question 4 ********
+
+// var tickPrice=600;
+// var noOfTick=5;
+// document.write("Total cost to buy "+ noOfTick +" is "+ tickPrice*noOfTick +"<br><br>");
+
+
+                // ********Question 5 ********
+
+// var tab=4;
+// for(var i=1;i<=10;i++){
+//     document.write( tab + "X" + i + "=" + tab*i+"<br>");
+// }document.write("<br>")
+
+
+                 // ********Question 6 ********
+
+// var celcius=25;
+// var fahrenheit=70;
+// var f=(celcius*9/5)+32;
+// var c=(fahrenheit-32)*5/9;
+// document.write(celcius+"<sup>o</sup>C is "+f+"<sup>o</sup>F"+"<br>");
+// document.write(fahrenheit+"<sup>o</sup>F is"+c+"<sup>o</sup>C"+"<br><br>");
+
+
+
+                 // ********Question 7 ********
+
+// var p1=650;
+// var p2=100;
+// var q1=3;
+// var q2=7;
+// total=(p1*q1)+(p2*q2)+100;
+// document.write("Price if item 1 is "+p1+"<br>");
+// document.write("Quantity of item 1 is "+q1+"<br>");
+// document.write("Price if item 2 is "+p2 +"<br>");
+// document.write("Quantity of item 2 is "+q2 +"<br>");
+// document.write("Shipping charges is 100" +"<br>");
+// document.write("Total cost of your item is "+total +"<br><br>");
+
+
+
+
+                 // ********Question 8 ********
+
+// var obtMarks=804;
+// var totMarks=980;
+// var percent=obtMarks/totMarks;
+// document.write("Obtain Marks:"+obtMarks +"<br>");
+// document.write("Total Marks:"+totMarks +"<br>");
+// document.write("Percentage:" + percent +"<br><br>");
+
+
+
+                 // ********Question 9 ********
+
+// var usDollars=10;
+// var sauRiyal=25;
+// var totPakRupee=10*104.80+25*28;
+// document.write("Total currency in PKR:"+totPakRupee+"<br><br>");
+
+
+                 // ********Question 10 ********
+
+// var num=6;
+// document.write("Arithmetic is " + num+5*10/2+"<br><br>");
+
+
+
+
+                 // ********Question 11 ********
+
+// var currentYear=2020;
+// var birthYear=1997;
+// var age=currentYear-birthYear;
+// document.write("Current year:"+currentYear+"<br>");
+// document.write("Birth year:"+birthYear+"<br>");
+// document.write("Your Age is:"+age+"<br><br>");
+
+
+
+                 // ********Question 12 ********
+
+// var radius=20;
+// var pi=3.142;
+// var circum=2*pi*radius;
+// var area=pi*(radius*radius);
+// document.write("Radius of Circle: "+radius+"<br>");
+// document.write("The circumference is: "+circum+"<br>");
+// document.write("The area is: "+area+"<br><br>");
+
+
+                 // ********Question 13 ********
+
+
+// var snacks="Chocolate chips";
+// var curAge=23;
+// var maxAge=65;
+// var snakPerDay=2;
+// document.write("Favourite Snack: "+snacks+"<br>");
+// document.write("Your current age "+curAge+"<br>");
+// document.write("Estimated maximum age "+maxAge+"<br>");
+// document.write("Amount of snacks per day "+snakPerDay+"<br>");
+// document.write("you will need "+(maxAge-curAge)*snakPerDay +" "+snacks+" to last you untill the rip old age of "+maxAge+"<br><br>"); 
